21237e06aa3b9eaaa17cc651526d6098515e0450
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-08-03 16:52:11 -0400

    Added fetch to render data

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import logo from "./logo.svg";
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      quote: [],
+      character: [],
+      error: false,
+    };
+  }
 
+  componentDidMount = () => {
+    this.setState({
+      loading: true,
+    });
+    fetch("https://the-office-api-11.herokuapp.com/")
+      .then((res) => res.json())
+      .then((quotes) => {
+        console.log(quotes);
+        this.setState({ quotes: quotes, loading: false });
+      })
+      .catch((error) => {
+        this.setState({
+          error: true,
+          loading: false,
+        });
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">the office.</header>
+        <p></p>
+      </div>
+    );
+  }
+}
 export default App;
