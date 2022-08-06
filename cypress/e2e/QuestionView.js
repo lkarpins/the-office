dacf4fc7eda57fef493b5ac2c779effb59efcad2
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-08-06 10:38:19 -0400

    Added quotes fixture

---
+++ cypress/e2e/QuestionView.js
@@ -0,0 +1,8 @@
+describe("Question View page", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://the-office-api-11.herokuapp.com/", {
+      fixture: "quotes",
+    });
+    cy.visit("http://localhost:3000/quotes");
+  });
+});
