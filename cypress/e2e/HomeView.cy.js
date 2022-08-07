ed1e9c46ea94e133df2d160acbbb33666437e0e2
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-08-07 11:07:19 -0400

    Added error handling tests

--- cypress/e2e/HomeView.cy.js
+++ cypress/e2e/HomeView.cy.js
@@ -1,5 +1,8 @@
 describe("Home view page", () => {
   beforeEach(() => {
+    cy.intercept("GET", "https://the-office-api-11.herokuapp.com/", {
+      fixture: "quotes",
+    });
     cy.visit("http://localhost:3000/");
   });
 
--- cypress/e2e/error-handling.cy.js
+++ cypress/e2e/error-handling.cy.js
@@ -1,5 +1,28 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Error View Page", () => {
+  beforeEach(() => {
+    cy.intercept("GET", "https://the-office-api-11.herokuapp.com/", {
+      fixture: "quotes",
+    });
+    cy.visit("http://localhost:3000/");
+  });
+
+  it("Should display error message when there is a server error", () => {
+    cy.intercept("GET", "https://the-office-api-11.herokuapp.com/", {
+      forceNetworkError: true,
+    })
+      .as("getNetworkFailure")
+      .visit("http://localhost:3000")
+      .wait("@getNetworkFailure")
+      .dataCy("error")
+      .contains("Uh oh! Something went wrong, please try again!");
+  });
+
+  it("Should display error message when there is a broken URL", () => {
+    cy.get('[data-cy="app-button"]')
+      .contains("Start Here")
+      .click()
+      .visit("http://localhost:3000/quotess")
+      .dataCy("error")
+      .contains("Uh oh! Something went wrong, please try again!");
+  });
+});
